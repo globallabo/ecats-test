d45725ce0f4f6ac915a757b3a7d30d5a1df01275
Christopher Bunn <github@thenerdery.org>
2021-10-06 19:10:05 +0900

    Add more dummy data questions to test different question types.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,46 @@ import Card from "./components/Card";
 const tempdata = [
   {
     id: 1,
-    instructionText: "Please choose the word that best completes the sentence.",
-    questionText: "English is ___.",
-    answerOptions: [
-      { answerText: "great", isCorrect: true },
-      { answerText: "good", isCorrect: false },
-      { answerText: "so-so", isCorrect: false },
-      { answerText: "bad", isCorrect: false },
-    ],
-  },
-  {
-    id: 2,
-    instructionText: "Please choose the word that best completes the sentence.",
+    instructionText: "Choose the correct answer to complete the text.",
     questionText: "Henry __ a good boy.",
     answerOptions: [
       { answerText: "are", isCorrect: false },
       { answerText: "be", isCorrect: false },
       { answerText: "been", isCorrect: false },
       { answerText: "is", isCorrect: true },
+    ],
+  },
+  {
+    id: 2,
+    instructionText: "Choose the correct sentence for the text to complete.",
+    questionText: "much",
+    answerOptions: [
+      { answerText: "I feel ___ better now.", isCorrect: true },
+      { answerText: "I ___ like this new computer.", isCorrect: false },
+      { answerText: "I like ___ spicy food.", isCorrect: false },
+      { answerText: "I sleep on the ___ weekends.", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    instructionText: "Choose the answer that cannot complete the text.",
+    questionText: "the best",
+    answerOptions: [
+      { answerText: "It was ___ movie I had ever seen.", isCorrect: false },
+      { answerText: "I had ___ time.", isCorrect: false },
+      { answerText: "That was ___ concert in a long time.", isCorrect: false },
+      { answerText: "___ really like the movie.", isCorrect: true },
+    ],
+  },
+  {
+    id: 4,
+    instructionText: "Choose the correct position for the text in parentheses.",
+    questionText: "These ① chicken wings ② are ③ spicy to ④ eat. (too)",
+    answerOptions: [
+      { answerText: "①", isCorrect: false },
+      { answerText: "②", isCorrect: false },
+      { answerText: "③", isCorrect: true },
+      { answerText: "④", isCorrect: false },
     ],
   },
 ];
