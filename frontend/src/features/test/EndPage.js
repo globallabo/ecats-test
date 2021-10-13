a9b1598c661147b8ea37cbcd5a3371e6442668eb
Christopher Bunn <github@thenerdery.org>
2021-10-13 19:51:05 +0900

    Create button to show/hide the results on the end page.

--- frontend/src/features/test/EndPage.js
+++ frontend/src/features/test/EndPage.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
 import Results from "./components/Results";
 
 export default function EndPage() {
+  const [showResults, setShowResults] = useState(false);
+  const onClick = () => setShowResults(true);
   return (
     <div>
-      <h2>Thanks for taking the ECATS Diagnostic Test</h2>
-      <Results />
+      <Box textAlign="center">
+        <h2>Thanks for taking the ECATS Diagnostic Test</h2>
+        {showResults ? (
+          <>
+            <Button variant="outlined" onClick={() => setShowResults(false)}>
+              Hide Results
+            </Button>
+            <Results />
+          </>
+        ) : (
+          <Button variant="outlined" onClick={() => setShowResults(true)}>
+            Show Results
+          </Button>
+        )}
+      </Box>
     </div>
   );
 }
