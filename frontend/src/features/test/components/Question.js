d1dd4243b4861ba3b25c65f46d68b06342fa95d6
Christopher Bunn <github@thenerdery.org>
2021-10-22 19:58:48 +0900

    Use dompurify to allow for simple HTML tags in question instructions (e.g. underline).

--- frontend/src/features/test/components/Question.js
+++ frontend/src/features/test/components/Question.js
@@ -1,9 +1,18 @@
 import Typography from "@mui/material/Typography";
+import dompurify from "dompurify";
 
 export default function Question({ question }) {
+  // Sanitize instruction text that might have simple HTML (underline, etc.)
+  const sanitizer = dompurify.sanitize;
   return (
     <>
-      <Typography variant="h2">{question.instructionText}</Typography>
+      <Typography variant="h2">
+        <span
+          dangerouslySetInnerHTML={{
+            __html: sanitizer(question.instructionText),
+          }}
+        />
+      </Typography>
       <Typography variant="body1">{question.questionText}</Typography>
     </>
   );
