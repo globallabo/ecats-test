e7e531ce47062bc1c1e437949e0f2731d45c9ae1
Christopher Bunn <github@thenerdery.org>
2021-10-22 19:56:44 +0900

    Replace English with Japanese and remove total number of questions.

--- frontend/src/features/test/components/Header.js
+++ frontend/src/features/test/components/Header.js
@@ -2,26 +2,18 @@ import { useSelector, useDispatch } from "react-redux";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-import {
-  selectQuestions,
-  selectCurrentQuestion,
-  handleAnswerButtonClick,
-} from "../testSlice";
+import { selectCurrentQuestion, handleAnswerButtonClick } from "../testSlice";
 import Timer from "./Timer";
 
 export default function Header() {
   const dispatch = useDispatch();
-  const questions = useSelector(selectQuestions);
-  const numQuestions = questions.length;
   const currentQuestion = useSelector(selectCurrentQuestion);
   const timerDuration = 30;
   const timeoutAnswer = { answerText: "TIMEOUT", isCorrect: false };
 
   return (
     <Box>
-      <Typography variant="h1">
-        Question {currentQuestion + 1} of {numQuestions}
-      </Typography>
+      <Typography variant="h1">問 {currentQuestion + 1}</Typography>
       <Timer
         duration={timerDuration}
         onTimeout={() => dispatch(handleAnswerButtonClick(timeoutAnswer))}
