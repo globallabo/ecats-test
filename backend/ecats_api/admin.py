3eaa40a4aee6ff1f7898ed1af7a3d4caf28ce55a
Christopher Bunn <github@thenerdery.org>
2021-11-03 21:50:28 +0900

    Create some admin views.

--- backend/ecats_api/admin.py
+++ backend/ecats_api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django import forms
+from django.db import models
 
-# Register your models here.
+from . import models as mymodels
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = mymodels.Answer
+    extra = 4
+    # fields =("answer_text", "is_correct")
+    formfield_overrides = {
+        models.TextField: {"widget": forms.widgets.TextInput(attrs={'size': 50})}
+    }
+
+
+@admin.register(mymodels.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("target", "question_type", "question_text")
+    inlines = [AnswerInlineModel]
+    formfield_overrides = {
+        models.TextField: {"widget": forms.widgets.TextInput(attrs={'size': 50})}
+    }
+
+@admin.register(mymodels.QuestionType)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("name", "instruction_text_en")
+
+@admin.register(mymodels.Target)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("level", "category", "subcategory", "can_do_statement_en")
+
+admin.site.register(mymodels.Level)
+admin.site.register(mymodels.Category)
+admin.site.register(mymodels.Subcategory)
