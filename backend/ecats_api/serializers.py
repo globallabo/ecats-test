acdbcd12958cbd25285b6cb014da088fb6fafe35
Christopher Bunn <github@thenerdery.org>
2021-11-10 18:16:03 +0900

    Add answer data to the question serializer to output answer data with each question to the api.

--- backend/ecats_api/serializers.py
+++ backend/ecats_api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from ecats_api import models
+from ecats_api.models import Question, Answer
 
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields =["id", "answer_text", "is_correct"]
 
 class QuestionSerializer(serializers.ModelSerializer):
 
+    answer_set = AnswerSerializer(many=True, read_only=True)
+
     class Meta:
-        model = models.Question
-        fields = ["id", "question_text"]
+        model = Question
+        fields = ["id", "question_text", "answer_set"]
