665f0e1f7cd41d0642f44d30f4fc2064a0d48ae9
Christopher Bunn <github@thenerdery.org>
2021-11-10 19:17:38 +0900

    Add question type data to the question serializer to output question type data with each question to the api.

--- backend/ecats_api/serializers.py
+++ backend/ecats_api/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from ecats_api.models import Question, Answer
+from ecats_api.models import Question, Answer, QuestionType
 
+
+class QuestionTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = QuestionType
+        fields =["id", "name", "instruction_text_en", "instruction_text_ja"]
 
 class AnswerSerializer(serializers.ModelSerializer):
 
@@ -10,8 +16,9 @@ class AnswerSerializer(serializers.ModelSerializer):
 
 class QuestionSerializer(serializers.ModelSerializer):
 
+    question_type = QuestionTypeSerializer(read_only=True)
     answer_set = AnswerSerializer(many=True, read_only=True)
 
     class Meta:
         model = Question
-        fields = ["id", "question_text", "answer_set"]
+        fields = ["id", "question_type", "question_text", "answer_set"]
