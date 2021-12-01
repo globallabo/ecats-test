dc44acc5fea689ba06857e2331d5a1eebcfcfbb3
Christopher Bunn <github@thenerdery.org>
2021-12-01 16:47:03 +0900

    Create RTK Query endpoints to match existing API endpoints for getting questions.

--- frontend/src/services/questions.js
+++ frontend/src/services/questions.js
@@ -1,6 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const questionApi = createApi({
-  reducerPath: "questionApi",
+export const questionsApi = createApi({
+  reducerPath: "questionsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/ecats/" }),
+  endpoints: (builder) => ({
+    getRandomQuestionByLevel: builder.query({
+      query: (level) => `r/${level}`,
+    }),
+    getAllQuestions: builder.query({
+      query: () => ``,
+    }),
+  }),
 });
+
+export const { useGetRandomQuestionByLevelQuery, useGetAllQuestionsQuery } =
+  questionsApi;
