e91ef585ee3e4d4b85e9f0cd4c0e0a0b5da5f7ef
Christopher Bunn <github@thenerdery.org>
2021-12-01 16:48:52 +0900

    Add RTK Query reducers and middleware to the store to allow querying and change to named export, not default.

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
+import { questionsApi } from "../services/questions";
 import testReducer from "../features/test/testSlice";
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     test: testReducer,
+    [questionsApi.reducerPath]: questionsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(questionsApi.middleware),
 });
+
+setupListeners(store.dispatch);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
-import store from "./app/store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
