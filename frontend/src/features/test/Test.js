4b896d367f5841652b41add841295319bbc5a89b
Christopher Bunn <github@thenerdery.org>
2021-12-01 16:52:39 +0900

    Replace the source of questions from the local testSlice state to the one from the API via RTK Query.

--- frontend/src/features/test/Test.js
+++ frontend/src/features/test/Test.js
@@ -2,11 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 
-import {
-  selectQuestions,
-  selectCurrentQuestion,
-  handleAnswerButtonClick,
-} from "./testSlice";
+import { useGetAllQuestionsQuery } from "../../services/questions";
+import { selectCurrentQuestion, handleAnswerButtonClick } from "./testSlice";
 
 import Option from "./components/Option";
 import Question from "./components/Question";
@@ -14,11 +11,20 @@ import Timer from "./components/Timer";
 
 export default function Test() {
   const dispatch = useDispatch();
-  const questions = useSelector(selectQuestions);
+  const {
+    data: questions = [],
+    isError,
+    isLoading,
+  } = useGetAllQuestionsQuery();
   const currentQuestion = useSelector(selectCurrentQuestion);
   const question = questions[currentQuestion];
   const timerDuration = 30;
   const timeoutAnswer = { answerText: "TIMEOUT", isCorrect: false };
+
+  // Check for errors or loading (might be a better way)
+  if (isError) return <div>Error!</div>;
+  if (isLoading) return <div>Loading ...</div>;
+  if (!questions) return <div>No data!.</div>;
 
   return (
     <Container>
