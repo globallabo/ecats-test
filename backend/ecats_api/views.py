9e45573fb94c1bbbeada2cdf86ba2d0bdf47a64c
Christopher Bunn <github@thenerdery.org>
2021-12-28 21:37:00 +0900

    Switch from API views to viewsets, which are simpler to manage.

--- backend/ecats_api/views.py
+++ backend/ecats_api/views.py
@@ -1,18 +1,33 @@
-from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from ecats_api.models import Question
-from ecats_api.serializers import QuestionSerializer, RandomQuestionSerializer
+from rest_framework import viewsets
+
+from ecats_api.models import TestTaker, Question
+from ecats_api.serializers import (
+    TestTakerSerializer,
+    QuestionSerializer,
+)
 
 
-class AllQuestions(generics.ListAPIView):
+class TestTakerViewSet(viewsets.ModelViewSet):
+    serializer_class = TestTakerSerializer
+
+    def get_queryset(self):
+        queryset = TestTaker.objects.all()
+        email = self.request.query_params.get("email")
+        if email is not None:
+            queryset = queryset.filter(email=email)
+        return queryset
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
-    queryset = Question.objects.all()
 
-
-class RandomQuestion(APIView):
-
-    def get(self, request, format=None, **kwargs):
-        question = Question.objects.filter(target__level=kwargs["level"]).order_by("?")[:1]
-        serializer = RandomQuestionSerializer(question, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        queryset = Question.objects.all()
+        level = self.request.query_params.get("level")
+        count = self.request.query_params.get("count")
+        if level is not None:
+            queryset = queryset.filter(target__level=level).order_by("?")
+        if count is not None:
+            count = int(count)
+            queryset = queryset[:count]
+        return queryset
