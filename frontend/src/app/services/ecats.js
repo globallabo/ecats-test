e1efdce95ff2c65cfd5e178186d7541a72004183
Christopher Bunn <github@thenerdery.org>
2021-12-29 23:19:29 +0900

    Create mutation for adding a test taker with a dummy example hard coded into a button to test it (BTW, Django needs a trailing slash on endpoint URLs).

--- frontend/src/app/services/ecats.js
+++ frontend/src/app/services/ecats.js
@@ -3,15 +3,34 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const ecatsApi = createApi({
   reducerPath: "ecatsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/ecats/" }),
+  tagTypes: ["Question", "TestTaker"],
   endpoints: (builder) => ({
     getRandomQuestionByLevel: builder.query({
       query: (level) => `r/${level}`,
+      providesTags: ["Question"],
     }),
     getAllQuestions: builder.query({
-      query: () => `questions`,
+      query: () => `questions/`,
+      providesTags: ["Question"],
+    }),
+    getAllTestTakers: builder.query({
+      query: () => `test_takers/`,
+      providesTags: ["TestTaker"],
+    }),
+    createTestTaker: builder.mutation({
+      query: (body) => ({
+        url: "test_takers/",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["TestTaker"],
     }),
   }),
 });
 
-export const { useGetRandomQuestionByLevelQuery, useGetAllQuestionsQuery } =
-  ecatsApi;
+export const {
+  useGetRandomQuestionByLevelQuery,
+  useGetAllQuestionsQuery,
+  useGetAllTestTakersQuery,
+  useCreateTestTakerMutation,
+} = ecatsApi;
--- frontend/src/features/admin/Admin.js
+++ frontend/src/features/admin/Admin.js
@@ -1,5 +1,47 @@
+import {
+  useCreateTestTakerMutation,
+  useGetAllTestTakersQuery,
+} from "../../app/services/ecats";
+
 function Admin() {
-  return <div>This is the admin page.</div>;
+  const {
+    data: testTakers = [],
+    isError,
+    isLoading,
+  } = useGetAllTestTakersQuery();
+
+  if (isError) return <div>Error!</div>;
+  if (isLoading) return <div>Loading ...</div>;
+  if (!testTakers) return <div>No data!.</div>;
+
+  return (
+    <>
+      <h1>Test Takers</h1>
+      {testTakers.map((testTaker) => (
+        <div key={testTaker.id}>
+          <p>
+            {testTaker.id}, {testTaker.email}, {testTaker.code}
+          </p>
+        </div>
+      ))}
+      <AddTestTaker />
+    </>
+  );
 }
+
+export const AddTestTaker = () => {
+  const [addTestTaker] = useCreateTestTakerMutation();
+  const testTaker = {
+    email: "something2@new.com",
+  };
+  const addHandler = async () => {
+    await addTestTaker(testTaker);
+  };
+  return (
+    <>
+      <button onClick={addHandler}>Add a new test taker</button>
+    </>
+  );
+};
 
 export default Admin;
