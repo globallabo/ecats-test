43b9e10e7d3372b1613a5f6deb8b5764fac3a89f
Christopher Bunn <github@thenerdery.org>
2021-12-30 03:35:56 +0900

    Create form component to add a new test taker with just an email address.

--- frontend/src/features/admin/Admin.js
+++ frontend/src/features/admin/Admin.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { TextField } from "@mui/material";
+
 import {
   useCreateTestTakerMutation,
   useGetAllTestTakersQuery,
@@ -10,6 +13,18 @@ function Admin() {
     isLoading,
   } = useGetAllTestTakersQuery();
 
+  const initialValue = { email: "" };
+  const [testTaker, setTestTaker] = useState(initialValue);
+  const handleChange = ({ target }) => {
+    setTestTaker((prev) => ({
+      ...prev,
+      [target.name]: target.value,
+    }));
+  };
+  const [addTestTaker] = useCreateTestTakerMutation();
+  const handleSubmit = () =>
+    addTestTaker(testTaker).then(() => setTestTaker(initialValue));
+
   if (isError) return <div>Error!</div>;
   if (isLoading) return <div>Loading ...</div>;
   if (!testTakers) return <div>No data!.</div>;
@@ -20,28 +35,38 @@ function Admin() {
       {testTakers.map((testTaker) => (
         <div key={testTaker.id}>
           <p>
-            {testTaker.id}, {testTaker.email}, {testTaker.code}
+            {testTaker.id}, {testTaker.email}, {testTaker.code},
+            {testTaker.active ? "Active" : "Inactive"}
           </p>
         </div>
       ))}
-      <AddTestTaker />
+      <TextField
+        onChange={handleChange}
+        value={testTaker.email}
+        required
+        fullWidth
+        id="test-taker-email"
+        name="email"
+        label="Email"
+      />
+      <button onClick={handleSubmit}>Add a new test taker</button>
     </>
   );
 }
 
-export const AddTestTaker = () => {
-  const [addTestTaker] = useCreateTestTakerMutation();
-  const testTaker = {
-    email: "something2@new.com",
-  };
-  const addHandler = async () => {
-    await addTestTaker(testTaker);
-  };
-  return (
-    <>
-      <button onClick={addHandler}>Add a new test taker</button>
-    </>
-  );
-};
+// export const AddTestTaker = () => {
+//   const [addTestTaker] = useCreateTestTakerMutation();
+//   const testTaker = {
+//     email: "something2@new.com",
+//   };
+//   const addHandler = async () => {
+//     await addTestTaker(testTaker);
+//   };
+//   return (
+//     <>
+//       <button onClick={addHandler}>Add a new test taker</button>
+//     </>
+//   );
+// };
 
 export default Admin;
