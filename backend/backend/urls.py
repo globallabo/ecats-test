80c354054b303f0491f4a7c4940b8024ee1d15c9
Christopher Bunn <github@thenerdery.org>
2022-04-11 22:48:54 +0900

    Change Django admin URL to avoid conflict with frontend admin page, and add rule to nginx to allow proxying of the Django URLs.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("ecats/", include("ecats_api.urls", namespace="ecats_api"))
+    path("djadmin/", admin.site.urls),
+    path("ecats/", include("ecats_api.urls", namespace="ecats_api")),
 ]
